a7eef1dbed08d1b768d89c44af759ebd44e433e1
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-08-25 07:58:13 -0600

    Dev #3

--- src/cmd/bshell.rs
+++ src/cmd/bshell.rs
@@ -1,7 +1,33 @@
 extern crate log;
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
 use crate::cmd;
 
-pub fn run_shell(_c: &cmd::Cli, _a: &Vec<String>) {
+pub fn run_shell(c: &cmd::Cli, _a: &Vec<String>)  {
     log::trace!("run_shell() reached");
+    let mut line = Editor::<()>::new().unwrap();
+    loop {
+        let readline = line.readline("[BUND> ");
+        match readline {
+            Ok(line) => {
+                shell_line(&c, &line)
+            },
+            Err(ReadlineError::Interrupted) => {
+                log::info!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                log::info!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                log::error!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+}
+
+fn shell_line(_c: &cmd::Cli, _line: &String) {
 
 }
