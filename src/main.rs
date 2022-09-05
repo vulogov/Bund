16898d9d30c9b5ff8cf114dca86c614ec8341ae1
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-09-05 13:13:16 -0600

    Dev #9

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod cmd;
 pub mod stdlib;
 pub mod lang;
 pub mod vm;
+pub mod twostack;
 
 fn main() {
     cmd::init();
---
+++ src/twostack/mod.rs
@@ -0,0 +1,30 @@
+use std::collections;
+use crate::vm::value;
+
+pub struct TS {
+    stack:       collections::VecDeque<collections::VecDeque<value::Value>>,
+}
+
+impl TS {
+    pub fn new() -> Self {
+        Self {
+            stack: collections::VecDeque::new(),
+        }
+    }
+}
+
+impl TS {
+    fn if_empty(&mut self) {
+        if self.stack.is_empty() {
+            self.stack.push_back(collections::VecDeque::new())
+        }
+    }
+    pub fn global(&mut self) -> &mut collections::VecDeque<collections::VecDeque<value::Value>> {
+        let _ = &mut self.if_empty();
+        &mut self.stack
+    }
+    pub fn local(&mut self) -> &mut collections::VecDeque<value::Value> {
+        let _ = &mut self.if_empty();
+        self.stack.back_mut().unwrap()
+    }
+}
