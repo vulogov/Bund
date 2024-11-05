9cf2781dc90fe4d842440a18b5caa9d31f9698fa
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-11-05 16:20:18 -0700

    Dev #34

--- src/stdlib/functions/bund/bund_world_bootstrap.rs
+++ src/stdlib/functions/bund/bund_world_bootstrap.rs
@@ -7,12 +7,21 @@ use rust_multistackvm::multistackvm::{VM};
 use rusqlite::{Connection};
 use easy_error::{Error, bail};
 
-
 pub fn bund_save_bootstrap_inline<'a>(vm: &'a mut VM, conn: &mut Connection, name: String, snippet: String) -> Result<&'a mut VM, Error> {
     match helpers::world::bootstrap::save_bootstrap(vm, conn, name, snippet) {
         Ok(_) => {}
         Err(err) => {
-            bail!("Bootstrap SAVE returns: {}", err)
+            bail!("Bootstrap SAVE.SCRIPT returns: {}", err)
+        }
+    }
+    Ok(vm)
+}
+
+pub fn bund_load_bootstrap_inline<'a>(vm: &'a mut VM, conn: &mut Connection, name: String) -> Result<&'a mut VM, Error> {
+    match helpers::world::bootstrap::load_bootstrap(vm, conn, name) {
+        Ok(_) => {}
+        Err(err) => {
+            bail!("Bootstrap LOAD.SCRIPT returns: {}", err)
         }
     }
     Ok(vm)
@@ -75,8 +84,53 @@ pub fn stdlib_bund_save_bootstrap(vm: &mut VM) -> Result<&mut VM, Error> {
     return res;
 }
 
-pub fn stdlib_bund_save_disabled(_vm: &mut VM) -> Result<&mut VM, Error> {
-    bail!("bund SAVE.BOOTSTRAP functions disabled with --noio");
+pub fn stdlib_bund_load_bootstrap(vm: &mut VM) -> Result<&mut VM, Error> {
+    if vm.stack.current_stack_len() < 2 {
+        bail!("Stack is too shallow for LOAD.SCRIPT");
+    }
+    let file_name_value = match vm.stack.pull() {
+        Some(file_name_value) => file_name_value,
+        None => {
+            bail!("LOAD.SCRIPT returns NO DATA #1");
+        }
+    };
+    let name_value = match vm.stack.pull() {
+        Some(name_value) => name_value,
+        None => {
+            bail!("LOAD.SCRIPT returns NO DATA #3");
+        }
+    };
+
+    let name = match name_value.cast_string() {
+        Ok(name) => name,
+        Err(err) => {
+            bail!("LOAD.SCRIPT casting string #3 returns: {}", err);
+        }
+    };
+    let file_name = match file_name_value.cast_string() {
+        Ok(file_name) => file_name,
+        Err(err) => {
+            bail!("LOAD.SCRIPT casting string #1 returns: {}", err);
+        }
+    };
+    let mut conn = match helpers::world::open(file_name) {
+        Ok(conn) => conn,
+        Err(err) => {
+            bail!("{}", err);
+        }
+    };
+    let res = bund_load_bootstrap_inline(vm, &mut conn, name);
+    match conn.close() {
+        Ok(_) => {},
+        Err(err) => {
+            log::debug!("Closing connection to the world returns error: {:?}", err);
+        }
+    }
+    return res;
+}
+
+pub fn stdlib_bund_save_script_disabled(_vm: &mut VM) -> Result<&mut VM, Error> {
+    bail!("bund [SAVE|LOAD].SCRIPT functions disabled with --noio");
 }
 
 pub fn init_stdlib(cli: &cmd::Cli) {
@@ -88,9 +142,11 @@ pub fn init_stdlib(cli: &cmd::Cli) {
         }
     };
     if cli.noio {
-        let _ = bc.vm.register_inline("save.bootstrap".to_string(), stdlib_bund_save_disabled);
+        let _ = bc.vm.register_inline("save.script".to_string(), stdlib_bund_save_script_disabled);
+        let _ = bc.vm.register_inline("load.script".to_string(), stdlib_bund_save_script_disabled);
     } else {
-        let _ = bc.vm.register_inline("save.bootstrap".to_string(), stdlib_bund_save_bootstrap);
+        let _ = bc.vm.register_inline("save.script".to_string(), stdlib_bund_save_bootstrap);
+        let _ = bc.vm.register_inline("load.script".to_string(), stdlib_bund_load_bootstrap);
     }
     drop(bc);
 }
--- src/stdlib/helpers/world/bootstrap.rs
+++ src/stdlib/helpers/world/bootstrap.rs
@@ -1,10 +1,57 @@
 extern crate log;
+use rust_dynamic::value::Value;
 use rusqlite::{Connection};
 use rust_multistackvm::multistackvm::{VM};
 use easy_error::{Error, bail};
 
+pub fn load_bootstrap<'a>(vm: &'a mut VM, conn: &mut Connection, name: String) -> Result<&'a mut VM, Error> {
+    let mut stmt = match conn.prepare("SELECT script FROM BOOTSTRAP WHERE name=?1") {
+        Ok(stmt) => stmt,
+        Err(err) => {
+            bail!("Error compiling BOOTSTRAP select: {:?}", err);
+        }
+    };
+    match stmt.query([name.clone(),]) {
+        Ok(mut rows) => {
+            loop {
+                match rows.next() {
+                    Ok(Some(row)) => {
+                        let script: String = match row.get(0) {
+                            Ok(script) => script,
+                            Err(err) => {
+                                bail!("Error getting script {}: {}", &name, err);
+                            }
+                        };
+                        let _ = vm.stack.push(Value::from_string(script));
+                    }
+                    Ok(None) => break,
+                    Err(err) => {
+                        log::debug!("Error getting SCRIPT row: {}", err);
+                        break;
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            bail!("Error performing SCRIPT select: {:?}", err);
+        }
+    }
+    Ok(vm)
+}
 
-pub fn save_bootstrap<'a>(vm: &'a mut VM, _conn: &mut Connection, _name: String, _snippet: String) -> Result<&'a mut VM, Error> {
+pub fn save_bootstrap<'a>(vm: &'a mut VM, conn: &mut Connection, name: String, snippet: String) -> Result<&'a mut VM, Error> {
+    match conn.execute("DELETE FROM BOOTSTRAP WHERE name=?1", (name.clone(),)) {
+        Ok(_) => {},
+        Err(err) => {
+            bail!("Deleting previous boostrap entry returns: {}", err);
+        }
+    }
+    match conn.execute("INSERT INTO BOOTSTRAP (name, script) VALUES (?1, ?2)", (name.clone(), snippet)) {
+        Ok(_) => {},
+        Err(err) => {
+            bail!("Creating boostrap entry returns: {}", err);
+        }
+    }
     Ok(vm)
 }
 
