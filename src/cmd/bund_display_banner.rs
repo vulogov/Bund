e76b338b717cc01e240325524b97622e01788a26
Vladimir Ulogov <vladimir.ulogov@me.com>
2024-11-07 18:39:12 -0700

    Dev #51

--- src/cmd/bund_display_banner.rs
+++ src/cmd/bund_display_banner.rs
@@ -1,9 +1,13 @@
-use neofiglet::FIGfont;
+use cfonts::{ render, Options, Fonts };
 
 pub fn banner(s: &String) -> String {
-    let standard_font = FIGfont::standard().unwrap();
-    let figure = standard_font.convert(&s);
-    figure.unwrap().to_string()
+    render(
+        Options {
+            text: String::from(s),
+            font: Fonts::FontPallet,
+            ..Options::default()
+        }
+    ).text
 }
 
 pub fn bund_banner() -> String {
